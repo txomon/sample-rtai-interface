9d0b0cb2f7409984cb9d60130b1bf879dfe3dcc2
Javier Domingo Cansino <javierdo1@gmail.com>
2015-05-15 17:53:38 +0200

    Starting to do the websocket thing, yet to see how to comunicate it properly with RTAI domain python script

---
+++ server.py
@@ -0,0 +1,34 @@
+import cherrypy
+from os.path import abspath
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import WebSocket
+
+cherrypy.config.update({'server.socket_port': 9000})
+WebSocketPlugin(cherrypy.engine).subscribe()
+cherrypy.tools.websocket = WebSocketTool()
+
+
+class WebSocketHandler(WebSocket):
+    def received_message(self, message):
+        return 'ok'
+
+class Root(object):
+    @cherrypy.expose
+    def ws(self):
+        handler = cherrypy.request.ws_handler
+
+
+cherrypy.quickstart(Root(), '/', config={
+    '/': {
+        'tools.staticfile.on': True,
+        'tools.staticfile.filename': abspath('./index.html')
+    },
+    '/app.js': {
+        'tools.staticfile.on': True,
+        'tools.staticfile.filename': abspath('./index.html')
+    },
+    '/ws': {
+        'tools.websocket.on': True,
+        'tools.websocket.handler_cls': WebSocketHandler,
+    },
+})
